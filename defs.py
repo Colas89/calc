1c105700b8b86ea3d28450643cb9933c1114e55e
Colas89 <45199081+Colas89@users.noreply.github.com>
2018-12-19 15:24:51 +0200

    defs.py

---
+++ defs.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+def minus (num1,num2):
+    return num1-num2
+
+def mul(num1,num2):
+    return num1*num2
+
+def add (num1,num2):
+    return num1+num2
+
+def del_slash (num1,num2):
+    return num1/num2
+
+def factorial (num1,num2):
+    return num1**num2
